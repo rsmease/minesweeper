4d5669c060d2a69d0f90708bb83d9206315197df
Ryan Mease <rsmease@gmail.com>
2017-10-05 15:40:11 -0700

    add tile class and reveal function

--- board.rb
+++ board.rb
@@ -6,7 +6,7 @@ class Board
   end
 
   def create_grid
-    Array.new(9) { Array.new(9, 0) }
+    Array.new(9) { Array.new(9, Tile.new) }
   end
 
   def render
@@ -17,9 +17,9 @@ class Board
   end
 
   def populate_mines
-    #NOTE: Does not check for redundancy 
+    #NOTE: Does not check for redundancy
     DIFFICULTY.times do
-      @grid[rand(@grid.length)][rand(@grid.length)] = 1
+      @grid[rand(@grid.length)][rand(@grid.length)].value = 2
     end
   end
 
@@ -30,7 +30,26 @@ class Board
   def []=(pos, value)
     @grid[pos[0]][pos[1]] = value
   end
+end
 
+class Tile
+  attr_accessor :value, :revealed
+  def initialize(revealed = false, value = 1)
+    @revealed = revealed
+    @value = value
+  end
+
+  def reveal
+    @revealed = true
+  end
+
+  def show
+    if @revealed
+      return @value
+    else
+      return 0
+    end
+  end
 end
 
 my_board = Board.new
